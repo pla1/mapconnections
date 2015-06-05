fd1eaae79cbf142d67ba2310d22cce6875a85d80
Patrick L Archibald <patrick.archibald@gmail.com>
2015-06-05 16:28:54 -0400

    Add examples in the comments.

--- tailfsyslog.sh
+++ tailfsyslog.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #
-# This is the script that you want websocketd to execute. 
+# This is the script that you want websocketd to execute.
 #
 # Example: ~/websocketd --port=8080 ./tailfsyslog.sh
+#
+# If you want to map connections on your headless web server
+# run the iptablesetup.sh on the web server and modify this
+# script on your workstation to use ssh.
+# Example: ssh webserver tailf /var/log/syslog
 #
 tailf /var/log/syslog
