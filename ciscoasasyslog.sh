632eeaa9a59c5fb0f9309bc79c3a33d75cd2b62b
Patrick L Archibald <patrick.archibald@gmail.com>
2015-06-06 19:30:52 -0400

    Update ciscoasasyslog.sh

--- ciscoasasyslog.sh
+++ ciscoasasyslog.sh
@@ -3,6 +3,6 @@
 # This will be the script called by websocketd when
 # mapping Cisco ASA firewall connections.
 # In the ASA config use command: logging host inside 10.1.2.3 udp/51414
-#
+# Replace 10.1.2.3 with your workstation IP address. 
 #
 nc -ul 51414
