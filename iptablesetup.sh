f6fccf6d42dd3a95253692e02a60a6588217c2c2
Patrick L Archibald <patrick.archibald@gmail.com>
2015-06-10 20:09:53 -0400

    Changed log prefix.

--- iptablesetup.sh
+++ iptablesetup.sh
@@ -2,6 +2,6 @@
 #
 # Log connections.
 #
-iptables -I INPUT -m state --state NEW -j LOG --log-prefix "New Connection: "
-iptables -I OUTPUT -m state --state NEW -j LOG --log-prefix "New Connection: "
+iptables -I INPUT -m state --state NEW -j LOG --log-prefix "New connection IN: "
+iptables -I OUTPUT -m state --state NEW -j LOG --log-prefix "New connection OUT: " --log-uid
 
